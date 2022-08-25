5de0667ba8976afa64a26ffdc6a1ebd522b4e20d
Rafael Viktor <rafaellviktor@gmail.com>
2022-08-25 17:56:20 -0300

    trabalho finalizado

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const crypto = require('crypto');
+const crypto = require('crypto'); // biblioteca que criptografa em sha256
 
+var qtdIteracoes = 2; // quantos blocos irá minerar (gerar hash válido), após o bloco genesis
+var caractereDificuldade = "0" // pode escolher o caractere
+var dificuldade = 1; // número de caracteres a ser validado no início do hash
 var nonce = 1;
 
 function Bloco(mensagem, indice, hashAnterior) {
@@ -15,14 +18,33 @@ function CalculaHash(bloco) {
         return hash;
 }
 
+// define a dificuldade da função abaixo dessa, de validação desses hashs gerados
+function DefineDificuldade(caractere, dificuldade) { // pode escolher o caractere e o número de caracteres a ser acrescentado a frente
+        let qtdCaracteres = [];
+        // array que vai juntar a quantidade de caracteres
+        for(let i = 1;i <= dificuldade; i++) {
+                qtdCaracteres.push(caractere);
+        }
+        // após criar array com todos os caracteres, juntar tudo em uma única string e retornar
+        let stringCaracteres = qtdCaracteres.join("");
+        return stringCaracteres;
+}
+
+let stringDificuldade = DefineDificuldade(caractereDificuldade,dificuldade);
+console.log(stringDificuldade)
+
 function ValidaHash(bloco) {
         let hashValido;
         do {
-                hashValido = CalculaHash(bloco);
-                nonce++;
+                if(bloco.indice == 0 && dificuldade == 0) {
+                        hashValido = CalculaHash(bloco); 
+                } else {
+                        nonce++;
+                        hashValido = CalculaHash(bloco);
+                }
                 //caso queira visualizar o console da validação só colocar console.log(hashValido) nessa parte
-        } while(hashValido.substring(0,1) !== "0")
-        // para mudar a condição só modificar acima
+        } while(hashValido.substring(0,dificuldade) !== stringDificuldade)
+        // acima os parâmetros de validação do hash gerado, coleta um pedaço da string do começo do hash e compara com a string de dificuldade
         return hashValido;
 }
 
@@ -38,8 +60,20 @@ function IniciarBlockchain() {
                 cadeiaHashs.push(ValidaHash(BlocoSeguinte));
                 console.log("[nonce] "+nonce)
                 indice++
-        } while (indice < 1)
+        } while (indice < qtdIteracoes)
+        cadeiaHashs.push('ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb')
         return cadeiaHashs;
 }
 
-console.log(IniciarBlockchain());
+function ValidaBlockchain(cadeiaHashs) {
+        // loop que percorre todo o array de hashs procurando hash inválido
+        for(let i = 0;i <= (qtdIteracoes); i++) {
+                if(cadeiaHashs[i].substring(0,dificuldade) !== stringDificuldade) {
+                        cadeiaHashs.splice(i,1,"|Erro|Hash Inválido|")
+                }
+        }
+
+        return cadeiaHashs;
+}
+
+console.log(ValidaBlockchain(IniciarBlockchain()));
